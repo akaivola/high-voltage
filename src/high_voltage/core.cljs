962f3b2090955c09034fb6a2a3e8be644d23552e
Aki Kaivola <aki.kaivola@gmail.com>
2018-08-11 13:41:12 +0300

    draw simple bars using views and css flexbox

--- src/high_voltage/core.cljs
+++ src/high_voltage/core.cljs
@@ -1,8 +1,9 @@
 (ns high-voltage.core
     (:require [reagent.core :as reagent :refer [atom]]
               [re-frame.core :as r :refer [subscribe dispatch dispatch-sync]]
-              [oops.core :as o]
+              [oops.core :as o :refer [oget]]
               [high-voltage.handlers]
+              [cuerdas.core :as str]
               [high-voltage.subs]))
 
 (def ReactNative (js/require "react-native"))
@@ -15,25 +16,69 @@
 (def text (reagent/adapt-react-class (.-Text ReactNative)))
 (def view (reagent/adapt-react-class (.-View ReactNative)))
 (def image (reagent/adapt-react-class (.-Image ReactNative)))
+(def StyleSheet (.-StyleSheet ReactNative))
 (def touchable-highlight (reagent/adapt-react-class (.-TouchableHighlight ReactNative)))
 (def Alert (.-Alert ReactNative))
 
 (defn alert [title]
   (.alert Alert title))
 
+(defn camelize-keys [m]
+  (clojure.walk/postwalk
+    (fn [k]
+      (if (keyword? k)
+        (str/camel (name k))
+        k))
+    m))
+
+(def style
+  (StyleSheet.create
+    (clj->js
+      (camelize-keys
+        {:gauges
+         {:width          50
+          :height         200}
+         :gauge
+         {:width            "100%"
+          :height           100
+          :flex-direction   "row"
+          :background-color "green"}
+         :gauge-positive
+         {:width            "100%"
+          :height           25
+          :align-self       "flex-end"
+          :background-color "rgb(0,255,0)"}
+         :gauge-negative
+         {:width            "100%"
+          :height           10
+          :align-self        "flex-start"
+          :background-color "rgb(255,0,0)"}
+         }))))
+
+(defn v [view-style & components]
+  (into
+    [view {:style (o/oget+ style (str/camel (name view-style)))}]
+    components))
+
 (defn app []
-  [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-   [image {:source (js/require "./assets/images/cljs.png")
+  [view {:style {:margin 40 :align-items "center"}}
+   #_[image {:source (js/require "./assets/images/cljs.png")
            :style  {:width  200
                     :height 200}}]
-   [text {:style
+   #_[text {:style
           {:font-size     30
            :font-weight   "100"
            :margin-bottom 20
            :font-family   "open-sans-bold"
            :text-align    "center"}}
     @(subscribe [:get-greeting])]
-   [ic {:name "ios-arrow-down" :size 60 :color "green"}]
+   [view [text "foosbars"]]
+   [v :gauges
+    [v :gauge
+     [v :gauge-positive]]
+    [v :gauge
+     [v :gauge-negative]]]
+   #_[ic {:name "ios-arrow-down" :size 60 :color "green"}]
    [touchable-highlight {:style    {:background-color "#999" :padding 10 :border-radius 5}
                          :on-press #(alert "HELLO")}
     [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])
